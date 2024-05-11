2cd97fea4379cc7d9036692bc55d025176d261ec
Kshitij <160639154+KshitijKhande@users.noreply.github.com>
2024-05-11 19:10:33 +0530

    Add files via upload

--- express/index.js
+++ express/index.js
@@ -1,8 +1,23 @@
-//Exampple of running a express server
 import express from "express";
+import bodyParser from "body-parser";
+
 const app=express();
 const port=3000;
 
-app.listen(port,() =>{
-    console.log(`The server is running on port ${port}`);
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+});
+
+app.post('/name',(req,res)=>{
+    const num=req.body['fname']+req.body['lname'];
+    const numletters=num.length;
+    res.render('index.ejs',{
+        title:`Your Name has ${numletters} letters`,
+    });
 })
+
+app.listen(port,()=>{
+    console.log(`This server is running on port ${port}`);
+});
